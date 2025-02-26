2177e9755511ae34cffd808a1822abd7144d99c7
Gustavo Quintero <gustavo141119@gmail.com>
2025-02-26 14:58:07 -0300

    New form factor in models script

--- models/models.py
+++ models/models.py
@@ -5,17 +5,18 @@ Created on Mon Feb 10 12:08:25 2025
 @author: gustavo
 """
 import numpy as np
-from scipy.special import j1  
-from scipy.integrate import quad_vec
-
 import matplotlib.pyplot as plt
+
+from scipy.special import j1  
+from scipy.special import sici
+from scipy.integrate import quad_vec
 
 def sphere(q, R, scale = 1, delta_rho = 1, background = 0.001):
     V = (4/3)*np.pi*R**3
     F = 3*(np.sin(q*R)-q*R*np.cos(q*R))/np.power(q*R, 3)
     return (scale/V)*(delta_rho**2)*(V**2)*(F*F) + background
 
-def core_shell_sphere(q, R1, R2, scale = 10, delta_rho = 1, background = 0.001):
+def core_shell_sphere(q, R1, R2, scale = 1, delta_rho = 1, background = 0.001):
     V1 = (4/3)*np.pi*R1**3
     V2 = (4/3)*np.pi*R2**3
     F1 = 3*(np.sin(q*R1)-q*R1*np.cos(q*R1))/np.power(q*R1, 3)
@@ -23,7 +24,7 @@ def core_shell_sphere(q, R1, R2, scale = 10, delta_rho = 1, background = 0.001):
     F  = (V1*F1-V2*F2)/(V1-V2)
     return  (scale/V1)*(delta_rho**2)*(V1**2)*(F*F) + background
 
-def cilynder(q, R, L, scale = 10, delta_rho = 1, background = 0.001):
+def cilinder(q, R, L, scale = 1, delta_rho = 1, background = 0.001):
     V = np.pi*L*R**2
     W = (scale/V)*(delta_rho**2)*(V**2)
     
@@ -72,27 +73,78 @@ def parallelepiped(q, a, b, c, scale = 1, delta_rho = 1, background = 0.001):
     P = quad_vec(outer_integral, 0, np.pi/2)[0]
     
     return W*P+background
-        
+
+def rod(q, L, scale = 1, delta_rho = 1, background = 0.001):
+    V = L
+    si, _ = sici(q*L)
+    P = 2*si/(q*L) - 4*np.power(np.sin(q*L/2),2)/np.power(q*L, 2)
+    
+    return P*(scale/V)*(delta_rho*V)**2 + background
+    
 if __name__ == "__main__":
     
-    q = np.linspace(0.001, 1, 1000)
+    q = np.linspace(0.001, 2, 1000)
+    
     R = 20
     R1 = 50
     R2 = 20
-    L = 400
-    Re = 400
-    Rp = 20
-    a = 35
+    L = 100
+    Re = 120
+    Rp = 40
+    a = 50
     b = 75
-    c = 400
-    #plt.plot(q, sphere(q, R))
-    #plt.plot(q, core_shell_sphere(q, R1, R2))
-    #plt.plot(q, cilynder(q, R, L))
-    #plt.plot(q, ellipsoid(q, Re, Rp))
-    plt.plot(q, parallelepiped(q, a, b, c))
-    plt.xlabel('q')
-    plt.ylabel('I(q)')
-    plt.title('Intesity of Parallelepiped')
-    plt.yscale('log')
-    plt.xscale('log')
+    c = 150
+    Lr = 100
+    
+    fig, axes = plt.subplots(nrows= 2, ncols= 3, constrained_layout=True)
+    
+    fig.suptitle('Scattering intensity for different form factors', fontweight ="bold")
+    
+    axes[0, 0].plot(q, sphere(q, R))
+    axes[0, 0].set_yscale('log')
+    axes[0, 0].set_xscale('log')
+    axes[0, 0].set_xlim([0.001, 2])
+    axes[0, 0].set_ylabel('I(q)')
+    axes[0, 0].set_xticks([])
+    axes[0, 0].set_title('Sphere')
+    
+    axes[0, 1].plot(q, core_shell_sphere(q, R1, R2))
+    axes[0, 1].set_yscale('log')
+    axes[0, 1].set_xscale('log')
+    axes[0, 1].set_xlim([0.001, 2])
+    axes[0, 1].set_xticks([])
+    axes[0, 1].set_yticks([])
+    axes[0, 1].set_title('Core shell sphere')
+    
+    axes[0, 2].plot(q, cilinder(q, R, L))
+    axes[0, 2].set_yscale('log')
+    axes[0, 2].set_xscale('log')
+    axes[0, 2].set_xlim([0.001, 2])
+    axes[0, 2].set_xticks([])
+    axes[0, 2].set_yticks([])
+    axes[0, 2].set_title('Cilinder')
+    
+    axes[1, 0].plot(q, ellipsoid(q, Re, Rp))
+    axes[1, 0].set_yscale('log')
+    axes[1, 0].set_xscale('log')
+    axes[1, 0].set_ylabel('I(q)')
+    axes[1, 0].set_xlabel('q(\u00C5\u207B\u00B9)')
+    axes[1, 0].set_xlim([0.001, 2])
+    axes[1, 0].set_title('Ellipsoid')
+    
+    axes[1, 1].plot(q, parallelepiped(q, a, b, c))
+    axes[1, 1].set_xscale('log')
+    axes[1, 1].set_xlim([0.001, 2])
+    axes[1, 1].set_yticks([])
+    axes[1, 1].set_xlabel('q(\u00C5\u207B\u00B9)')
+    axes[1, 1].set_title('Parallelepiped')
+    
+    axes[1, 2].plot(q, rod(q, L))
+    axes[1, 2].set_yscale('log')
+    axes[1, 2].set_xscale('log')
+    axes[1, 2].set_xlim([0.001, 2])
+    axes[1, 2].set_yticks([])
+    axes[1, 2].set_title('Rod')
+    axes[1, 2].set_xlabel('q(\u00C5\u207B\u00B9)')
+
     plt.show()
